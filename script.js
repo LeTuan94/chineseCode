a633dfaf6a6eaf632c43e3d5ba9eb5cd06498e5f
LeTuan94 <49394092+LeTuan94@users.noreply.github.com>
2026-04-06 18:48:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -263,18 +263,19 @@ function scrollToActiveRow() {
 }
 
 /* ================= LOAD JSON HSK ================= */
+const basePath = window.location.pathname;
 const hskFiles = {
-    "HSK 1": "https://learnforfree.fun/data/hsk1.json",
-    "HSK 2": "https://learnforfree.fun/data/hsk1.json",
-    "HSK 3": "https://learnforfree.fun/data/hsk1.json",
-    "HSK 4": "https://learnforfree.fun/data/hsk1.json",
-    "HSK 5": "https://learnforfree.fun/data/hsk1.json",
-    "HSK 6": "https://learnforfree.fun/data/hsk1.json",
-    "HSK 1 (3.0)": "/data/hsk1_3.json",
-    "HSK 2 (3.0)": "/data/hsk2_3.json",
-    "HSK 3 (3.0)": "/data/hsk3_3.json",
-    "HSK 4 (3.0)": "/data/hsk4_3.json",
-    "HSK 5 (3.0)": "/data/hsk5_3.json"
+    "HSK 1": "https://letuan94.github.io/chineseCode/data/hsk1.json",
+    "HSK 2": "https://letuan94.github.io/chineseCode/data/hsk2.json",
+    "HSK 3": "https://letuan94.github.io/chineseCode/data/hsk3.json",
+    "HSK 4": "https://letuan94.github.io/chineseCode/data/hsk4.json",
+    "HSK 5": "https://letuan94.github.io/chineseCode/data/hsk5.json",
+    "HSK 6": "https://letuan94.github.io/chineseCode/data/hsk6.json",
+    "HSK 1 (3.0)": "https://letuan94.github.io/chineseCode/data/hsk1_3.json",
+    "HSK 2 (3.0)": "https://letuan94.github.io/chineseCode/data/hsk2_3.json",
+    "HSK 3 (3.0)": "https://letuan94.github.io/chineseCode/data/hsk3_3.json",
+    "HSK 4 (3.0)": "https://letuan94.github.io/chineseCode/data/hsk4_3.json",
+    "HSK 5 (3.0)": "https://letuan94.github.io/chineseCode/data/hsk5_3.json"
 };
 
 async function loadHSK(level){
