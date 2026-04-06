ff20be4d2338026e8c2f2c5c048ab34a06a9de08
LeTuan94 <49394092+LeTuan94@users.noreply.github.com>
2026-04-06 19:24:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -265,8 +265,8 @@ function scrollToActiveRow() {
 /* ================= LOAD JSON HSK ================= */
 const basePath = window.location.pathname;
     const hskFiles = {
-    "HSK 1": "data/HSK1.json",
-    "HSK 2": "/data/HSK2.json",
+    "HSK 1": "data/hsk1.json",
+    "HSK 2": "/data/hsk2.json",
     "HSK 3": "data/hsk3.json",
     "HSK 4": "data/hsk4.json",
     "HSK 5": "data/hsk5.json",
